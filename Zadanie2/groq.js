211b6bcc45327ef82b0910e78323d0c1c7b39549
stejzy <247792@edu.p.lodz.pl>
2024-10-20 23:46:46 +0200

    Modify chat prompt

--- Zadanie2/groq.js
+++ Zadanie2/groq.js
@@ -8,7 +8,7 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 export async function categorizeTask(title, description) {
   const chatCompletion = await getGroqChatCompletion(title, description);
   console.log("Response from Groq API:", chatCompletion);
-  return chatCompletion.choices[0]?.message?.content || 'Uncategorized';
+  return chatCompletion.choices[0]?.message?.content || 'Uncategorized2';
 }
 
 async function getGroqChatCompletion(title, description) {
@@ -16,7 +16,18 @@ async function getGroqChatCompletion(title, description) {
     messages: [
       {
         role: "user",
-        content: `Categorize this task based on its title and description:\nTitle: "${title}"\nDescription: "${description}"`,
+        content: `Categorize this task based on its title and description:\nTitle: "${title}"\nDescription: "${description}"\n\nAvailable categories:
+        \n Praca
+        \n Studia
+        \n Zakupy
+        \n Zdrowie i Fitness
+        \n Hobby
+        \n Dom i Ogród
+        \n Spotkania towarzyskie
+        \n Projekty Osobiste
+        \n Finanse
+        \n Rozwój osobisty
+        \nPlease respond with categories listed above not number only for the category, or 'Uncategorized' if you cannot determine the category.`,
       },
     ],
     model: "llama3-8b-8192", // Change this to the model you want to use
